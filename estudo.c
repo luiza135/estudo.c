3a0d074cd007cbf9ec672afb939a4a24539f9c35
luiza135 <luizaalmeidaferreira10650@outlook.com>
2025-06-11 19:49:56 -0300

    Create estudo.c

---
+++ estudo.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h> 
+/*
+precisa incluir essa # pois é a bilioteca padrão para 
+identificar meu comando na tela, como printf e scanf 
+*/
+int main()
+//toda aplicação em C inicia com int main() obrigatóriamente
+{printf("Olá, Mundo Lindo, cheio de novas oportunidades!\n");
+// o \n indica para adicionar mais uma linha na hora que for imprimir;
+return 0;
+// return é pq o programa voltou com sucesso na hoa de imprimir
+}
+// essa é a base da programação em C, ao final de cada () eu adiciono ; e ao finalizar também.
